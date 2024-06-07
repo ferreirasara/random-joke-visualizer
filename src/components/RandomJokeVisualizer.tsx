d9e175fb65d1f278712600756f0334822c56f8b5
Sara Cristina Ferreira <ferreirasara1501@gmail.com>
2024-06-07 14:50:14 -0300

    fix: try with tsc

--- src/components/RandomJokeVisualizer.tsx
+++ src/components/RandomJokeVisualizer.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 interface Joke {
   type: string;
